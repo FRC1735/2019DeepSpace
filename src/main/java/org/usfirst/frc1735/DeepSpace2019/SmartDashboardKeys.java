e010a40a0478d62674d3bc3d5f1d4cfd6a038bff
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2019-01-19 10:49:25 -0500

    Implemennt EnterArcadeMode command

--- src/main/java/org/usfirst/frc1735/DeepSpace2019/SmartDashboardKeys.java
+++ src/main/java/org/usfirst/frc1735/DeepSpace2019/SmartDashboardKeys.java
@@ -2,4 +2,5 @@ package org.usfirst.frc1735.DeepSpace2019;
 
 public class SmartDashboardKeys {
     public static final String JOYSTICK_DEADZONE = "Joystick Deadzone";
+    public static final String DRIVETRAIN_MODE = "Drivetrain Mode";
 }
